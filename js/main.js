b46f36e7ca2e04190d8e2e5eb3e8499bc76453bc
JESJ <jerry.salandy@bt.com>
2013-09-11 21:14:31 +0100

    Adding code comments and slightly more optimized code
    Next I will make this look more attractive

--- js/main.js
+++ js/main.js
@@ -1,19 +1,29 @@
 $(document).ready(function(){
+	/*Basic validation function to:
+	1) Ensure submitted value is not empty
+	2) Ensure a valid numeric value is entered
+	Once all is well, pass on to penceCalculator function*/
 	var validate = function(totalP){		
 		var totalPence = totalP;
 		
 		if($.trim(totalPence) == ""){
-			alert("Please enter a value");
+			$('#txtPence').addClass("error");
+			$('#errorCanvas').fadeIn(500).text("Please enter the amount of pennies above. E.G £2.49 or 249");
 		}
 		else if(!$.isNumeric(totalPence)){
-			alert("Please enter a valid number");
+			$('#txtPence').addClass("error");
+			$('#errorCanvas').fadeIn(500).text("Sorry, I only accept numeric values. E.G £2.49 or 249");
 		}
 		else{
+			$('#txtPence').removeClass("error");
+			$('#errorCanvas').fadeOut(500);
 			penceCalculator(totalPence);
 		}		
 	};
 	
+	/*This function calculates the total sterling coins needed to make the pence amount entered */
 	var penceCalculator = function(totalPence){
+		// Sterling denomination object, holds the accepted sterling denomination valuse and displays for this project
 		var denom = {
 		  	twoPounds: {display: "£2", value: 200},
 		  	onePound: {display: "£1", value: 100},
@@ -25,24 +35,39 @@ $(document).ready(function(){
 		  	oneP: {display: "1p", value: 1}
 		};
 		
+		//Series of assignments to clean the value entered for simpler calculation
+		totalPence = totalPence.replace(/[.]/g, "");
 		totalPence = parseFloat(totalPence);
 		totalPence = totalPence.toFixed(2);
-		totalPence = totalPence.replace(/[.]/g, "");
-		  
+		
+		
+		//Itterate through the denominations and compare values against the entered amount
+		//Take the remainder and re-assign to pence value. 
+		//Repeat until end of denominations
 		for(var key in denom){
 			denom.obj = denom[key];
 		  	
 		  	if(totalPence >= denom.obj.value){
 		  		calcVal = totalPence / denom.obj.value;
-		  		console.log(denom.obj.display + " " + Math.floor(calcVal));
+				$('#resultCanvas ul').append("<li><strong>"+denom.obj.display +"</strong> " + Math.floor(calcVal)+"</li>");
 		  		totalPence = totalPence % denom.obj.value;
 		  	}
 		}
 	};
 	
+	//Detect form entry and kick off validation
 	$('#btnCalcPenc').click(function(){
+		$('#resultCanvas ul').empty();
 		var totalP = $('#txtPence').val().replace(/[£]/g, "");
 		validate(totalP);
+	});
+	
+	//Trigger button click if enter is pressed
+	$("input").keypress(function(event){
+		if (event.which == 13){
+			event.preventDefault();
+			$('#btnCalcPenc').trigger('click');
+		}
 	});
 	
 });
